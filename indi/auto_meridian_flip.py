14e8048009d23f08f305362d35e5bacfb3450b1e
Joydeep Biswas <joydeepb@cs.utexas.edu>
2024-01-21 16:00:27 -0600

    Updated INDI scripts

---
+++ indi/auto_meridian_flip.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+
+import sys
+import time
+import argparse
+import subprocess
+import astropy.time
+from astropy.coordinates import SkyCoord
+
+
+def exec(command):
+  # print(command)
+  # Execute the command, and check the return code.
+  returncode = subprocess.call(command, shell=True)
+  if returncode != 0:
+    print("Error: command '%s' returned %d" % (command, returncode))
+    sys.exit(1)
+
+def ReadIndi(device, propname):
+  # Call indi_getprop to get the property value
+  command = "indi_getprop \"%s.%s\"" % (device, propname)
+  # Execute the command and get the output.
+  output = subprocess.run(command, shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
+  # Parse the output to get the property value.
+  output = output.split("=")[1].strip()
+  return output
+
+def indi_goto(device, ra, dec):
+  command = "indi_setprop \"%s.EQUATORIAL_EOD_COORD.RA=%f;DEC=%f\"" % (device, ra, dec)
+  exec("indi_setprop \"%s.ON_COORD_SET.TRACK=On\"" % device)
+  exec(command)
+
+def target_wait(device):
+  while True:
+    target_ra = float(ReadIndi(device, "TARGET_EOD_COORD.RA"))
+    target_dec = float(ReadIndi(device, "TARGET_EOD_COORD.DEC"))
+    ra = float(ReadIndi(device, "EQUATORIAL_EOD_COORD.RA"))
+    dec = float(ReadIndi(device, "EQUATORIAL_EOD_COORD.DEC"))
+    if abs(target_ra - ra) < 0.001 and abs(target_dec - dec) < 0.001:
+      return
+    print("Target: %9.6f %9.6f Current: %9.6f %9.6f Difference: %9.6f %9.6f" % (target_ra, target_dec, ra, dec, target_ra - ra, target_dec - dec))
+    time.sleep(1)
+
+def perform_meridian_flip(device):
+  target_ra = float(ReadIndi(device, "TARGET_EOD_COORD.RA"))
+  target_dec = float(ReadIndi(device, "TARGET_EOD_COORD.DEC"))
+  indi_goto(device, target_ra, target_dec)
+  time.sleep(1)
+  target_wait(device)
+
+def main():
+  parser = argparse.ArgumentParser(description='Read site details from an INDI device')
+  parser.add_argument('-d', '--device', type=str, help='INDI device name', default='SkyAdventurer GTi')
+  parser.add_argument('-a', '--flip-angle', type=float, help='Hour angle flip angle', default=1/60)
+  
+  args = parser.parse_args()
+  print("Using device %s" % args.device)
+
+  while True:
+    target_wait(args.device)
+    ra = float(ReadIndi(args.device, "EQUATORIAL_EOD_COORD.RA"))
+    dec = float(ReadIndi(args.device, "EQUATORIAL_EOD_COORD.DEC"))
+    lst = float(ReadIndi(args.device, "TIME_LST.LST"))
+    # Convert RA,DEC to HA,DEC.
+    ha = lst - ra
+    time_to_flip = (args.flip_angle - ha) * 3600
+    time_to_flip_hours = time_to_flip / 3600
+    time_to_flip_minutes = (time_to_flip % 3600) / 60
+    time_to_flip_seconds = time_to_flip % 60
+    print("RA: %9.6f HA: %9.6f Time to flip: %02d:%02d:%02d" % (ra, ha, time_to_flip_hours, time_to_flip_minutes, time_to_flip_seconds))
+    if ha > args.flip_angle:
+      print("Performing meridian flip")
+      perform_meridian_flip(args.device)
+      print("Meridian flip complete")
+      sys.exit(0)
+
+  
+    time.sleep(1)
+
+if __name__ == "__main__":
+  main()
--- indi/read_site.py
+++ indi/read_site.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
-import sys
-import os
+import time
+import argparse
 import subprocess
 import astropy.time
+from astropy.coordinates import SkyCoord
 
 def ReadIndi(device, propname):
   # Call indi_getprop to get the property value
@@ -15,28 +16,37 @@ def ReadIndi(device, propname):
   return output
 
 def main():
-
-  # Optional parameter: device name
-  if len(sys.argv) > 1:
-    device = sys.argv[1]
-  else:
-    device = "SkyAdventurer GTi"
+  parser = argparse.ArgumentParser(description='Read site details from an INDI device')
+  parser.add_argument('-d', '--device', type=str, help='INDI device name', default='SkyAdventurer GTi')
+  parser.add_argument('-t', '--track-status', action='store_true', help='Print tracking status')
   
-  latitude = float(ReadIndi(device, "GEOGRAPHIC_COORD.LAT"))
-  longitude = float(ReadIndi(device, "GEOGRAPHIC_COORD.LONG"))
-  elevation = float(ReadIndi(device, "GEOGRAPHIC_COORD.ELEV"))
-  site_juliandate = float(ReadIndi(device, "JULIAN.JULIANDATE"))
+  args = parser.parse_args()
+  latitude = float(ReadIndi(args.device, "GEOGRAPHIC_COORD.LAT"))
+  longitude = float(ReadIndi(args.device, "GEOGRAPHIC_COORD.LONG"))
+  elevation = float(ReadIndi(args.device, "GEOGRAPHIC_COORD.ELEV"))
+  site_juliandate = float(ReadIndi(args.device, "JULIAN.JULIANDATE"))
   current_juliandate = float(astropy.time.Time.now().jd)
   juliandate_error_seconds = (current_juliandate - site_juliandate) * 86400
 
-  print("Site details for %s:" % device)
+  print("Site details for %s:" % args.device)
   print("Latitude:              %9.3f" % latitude)
   print("Longitude:             %9.3f" % longitude)
   print("Elevation:             %8.2f" % elevation)
   print("Site Julian Date:      %.7f" % site_juliandate)
-  print("Site UTC Time:         %s" % ReadIndi(device, "TIME_UTC.UTC"))
+  print("Site UTC Time:         %s" % ReadIndi(args.device, "TIME_UTC.UTC"))
   print("Current Julian Date:   %.7f" % current_juliandate)
   print("Julian Date Error (s): %.3f" % juliandate_error_seconds)
+
+  if args.track_status:
+    print("Tracking status")
+    while True:
+      ra = float(ReadIndi(args.device, "EQUATORIAL_EOD_COORD.RA"))
+      dec = float(ReadIndi(args.device, "EQUATORIAL_EOD_COORD.DEC"))
+      lst = float(ReadIndi(args.device, "TIME_LST.LST"))
+      # Convert RA,DEC to HA,DEC.
+      ha = lst - ra
+      print("RA: %9.6f DEC: %9.3f HA: %9.6f" % (ra, dec, ha))
+      time.sleep(1)
 
 if __name__ == "__main__":
   main()
