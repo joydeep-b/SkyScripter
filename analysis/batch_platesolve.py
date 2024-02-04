1bce3b61c3ba300189628af226dabed63679ce51
Joydeep Biswas <joydeepb@cs.utexas.edu>
2024-02-04 08:55:20 -0600

    Analysis

--- analysis/batch_platesolve.py
+++ analysis/batch_platesolve.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from astroquery.simbad import Simbad
 from astropy.coordinates import SkyCoord
 import astropy.units as units
@@ -6,6 +8,13 @@ import os
 import sys
 import subprocess
 import re
+import tempfile
+import matplotlib.pyplot as plt
+
+if sys.platform == 'darwin':
+  SIRIL_PATH = '/Applications/Siril.app/Contents/MacOS/Siril'
+else:
+  SIRIL_PATH = 'siril-cli'
 
 def extract_and_convert_coordinates_astap(output):
     # Define the regex pattern, to match output like this:
@@ -75,46 +84,42 @@ def get_wcs_coordinates(object_name):
     return coord.ra.deg, coord.dec.deg
 
 def run_star_detect_siril(this_dir, file):
-    # If MacOS, use the Siril.app version
-    if sys.platform == 'darwin':
-      SIRIL_PATH = '/Applications/Siril.app/Contents/MacOS/siril-cli'
-    else:
-      SIRIL_PATH = 'siril-cli'
-      
+    global SIRIL_PATH
     siril_commands = f"""requires 1.2.0
-load {file}
+convertraw light -debayer -out=.
+load light_00001
 findstar
 close
 """
-    # Define the command to run
-    siril_cli_command = [SIRIL_PATH, "-d", this_dir, "-s", "-"]
-
-    # Run the command and capture output
-    try:
-        result = subprocess.run(siril_cli_command, 
-                                input=siril_commands,
-                                text=True, 
-                                capture_output=True,
-                                check=True)
-        # Extract the number of stars detected, and the FWHM. Sample output:
-        # Found 343 Gaussian profile stars in image, channel #0 (FWHM 5.428217)
-        regex = r"Found ([0-9]+) Gaussian profile stars in image, channel #0 \(FWHM ([0-9]+\.[0-9]+)\)"
-        match = re.search(regex, result.stdout)
-        if not match:
-            print("No match found")
+    # Create a temp directory for Siril to use.
+    with tempfile.TemporaryDirectory() as temp_dir:
+        # Copy the file to the temp directory.
+        temp_file = temp_dir + '/' + os.path.basename(file)
+        os.system(f"cp {file} {temp_file}")
+        # Define the command to run
+        siril_cli_command = [SIRIL_PATH, "-d", temp_dir, "-s", "-"]
+        # Run the command and capture output
+        try:
+            result = subprocess.run(siril_cli_command, 
+                                    input=siril_commands,
+                                    text=True, 
+                                    capture_output=True,
+                                    check=True)
+            # Extract the number of stars detected, and the FWHM. Sample output:
+            # Found 343 Gaussian profile stars in image, channel #1 (FWHM 5.428217)
+            regex = r"Found ([0-9]+) Gaussian profile stars in image, channel #[0-2] \(FWHM ([0-9]+\.[0-9]+)\)"
+            # print(result.stdout)
+            match = re.search(regex, result.stdout)
+            if not match:
+                print("No match found")
+                return None, None
+            num_stars, fwhm = match.groups()
+            return num_stars, fwhm
+        except subprocess.CalledProcessError as e:
             return None, None
-        num_stars, fwhm = match.groups()
-        return num_stars, fwhm
-    except subprocess.CalledProcessError as e:
-        return None, None
     
 def run_plate_solve_siril(this_dir, file, wcs_coords, focal_option):
-    # If MacOS, use the Siril.app version
-    if sys.platform == 'darwin':
-      SIRIL_PATH = '/Applications/Siril.app/Contents/MacOS/siril-cli'
-    else:
-      SIRIL_PATH = 'siril-cli'
-
+    global SIRIL_PATH
     siril_commands = f"""requires 1.2.0
 load {file}
 platesolve {wcs_coords} -platesolve -catalog=nomad -limitmag=10 {focal_option}
@@ -148,6 +153,50 @@ def run_plate_solve_astap(this_dir, file, wcs_coords, focal_option):
     except subprocess.CalledProcessError as e:
         return None, None
     
+def load_prev_files(filename):
+    if not os.path.exists(filename):
+        return [], []
+    # The file is a CSV file with several columns, with the filename in the first column.
+    filenames = []
+    star_stats = []
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        for line in lines[1:]:
+            print(line)
+            parts = line.split(',')
+            filenames.append(parts[0])
+            star_stats.append((int(parts[3]), float(parts[4])))
+    return filenames, star_stats
+    
+def plot_star_stats(data):
+    # print(data)
+    # Unpack the data into two separate lists
+    num_stars, fwhm = zip(*data)
+
+    # Create a figure and a single subplot
+    fig, ax1 = plt.subplots()
+
+    # Plot num_stars on the left y-axis
+    color = 'tab:red'
+    ax1.set_xlabel('Data Point')
+    ax1.set_ylabel('num_stars', color=color)
+    ax1.plot(num_stars, color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    # Create a second y-axis for FWHM
+    ax2 = ax1.twinx()
+    color = 'tab:blue'
+    ax2.set_ylabel('FWHM', color=color)
+    ax2.plot(fwhm, color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+    # Set Y axis labels to show 2 decimal places.
+    ax2.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{x:.2f}'))
+
+    # Set the title.
+    plt.title('Number of stars detected and FWHM')
+    # Show the plot
+    plt.show()
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Platesolve all images in a directory')
     parser.add_argument('-d', '--directory', type=str, help='Directory containing images to platesolve')
@@ -185,11 +234,21 @@ if __name__ == "__main__":
     if coordinates is None:
         print('\nWARNING!\nBlind platesolving using ASTAP, no WCS coordinates specified, and no object name specified -- this will be slow! \nIf you know the approximate RA and DEC of the image, specify it with the -w option, or specify an object name with the -o option.\n')
 
+    prev_filenames, star_stats = load_prev_files(args.csv)
+    if len(prev_filenames) > 0:
+        print(f"Skipping {len(prev_filenames)} files that have already been platesolved")
+        if False:
+            n = len(prev_filenames)
+            for i in range(n):
+                print(f"{prev_filenames[i]}: {star_stats[i]}")
+            exit(1)
+
     current_dir = os.getcwd()
     csv_file = None
     if args.csv != '':
-        csv_file = open(args.csv, 'w')
-        csv_file.write('Filename,RA,DEC,NumStars,FWHM\n')
+        csv_file = open(args.csv, 'a')
+        if len(prev_filenames) == 0:
+            csv_file.write('Filename,RA,DEC,NumStars,FWHM\n')
         print(f"Writing results to {args.csv}")
     # Run platesolve on all images in the directory
     for filename in sorted(os.listdir(args.directory)):
@@ -199,6 +258,9 @@ if __name__ == "__main__":
         # Exclude non-image files.
         allowed_extensions = ['.fit', '.fits', '.cr2', '.cr3', '.jpg', '.png', '.tif', '.tiff']
         if not any(filename.lower().endswith(ext) for ext in allowed_extensions):
+            continue
+        if filename in prev_filenames:
+            print(f"Skipping {filename}")
             continue
         file = args.directory + '/' + filename
         if coordinates is None:
@@ -211,8 +273,14 @@ if __name__ == "__main__":
         else:
             print(f"Platesolve failed")
         num_stars, fwhm = run_star_detect_siril(current_dir, file)
+        star_stats.append((int(num_stars), float(fwhm)))
         print(f"File: {filename}, RA={result[0]:.12f}, DEC={result[1]:.12f}, NumStars={num_stars}, FWHM={fwhm}")
         if csv_file is not None:
             # Write the numbers in 6 decimal places
             csv_file.write(f"{filename}, {result[0]:.12f}, {result[1]:.12f}, {num_stars}, {fwhm}\n")
         # sys.exit(1)
+    # Create a 2D scatter plot of the number of stars detected vs. FWHM.
+    if csv_file is not None:
+        csv_file.close()  
+    plot_star_stats(star_stats)
+    
