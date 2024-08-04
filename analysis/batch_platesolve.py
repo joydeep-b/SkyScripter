d8ec18515c051d6c2fc2ea59fd4bf3ded79b9582
Joydeep Biswas <joydeepb@cs.utexas.edu>
2024-08-04 18:03:45 -0500

    Better batch platesolve

--- analysis/batch_platesolve.py
+++ analysis/batch_platesolve.py
@@ -87,12 +87,18 @@ def get_wcs_coordinates(object_name):
     # Return RA, DEC in degrees
     return coord.ra.deg, coord.dec.deg
 
-def run_star_detect_siril(this_dir, file):
+def findstar_and_platesolve_siril(wcs_coords: str, file: str)  -> (tuple[int, float, float | None, float | None] | tuple[None, None, None, None]):
     global SIRIL_PATH
+    if wcs_coords is None:
+        platesolve_command = 'platesolve'
+    else:
+        platesolve_command = f'platesolve {wcs_coords}'
     siril_commands = f"""requires 1.2.0
-convert light -debayer -out=.
-load light_00001
+convert light -out=.
+calibrate_single light_00001 -dark=/Users/joydeepbiswas/Astrophotography/masters/dark/master_dark_MODE$READMODE:%1d$_GAIN$GAIN:%2d$_OFFSET$OFFSET:%2d$_EXPTIME$EXPTIME:%3d$ -flat=/Users/joydeepbiswas/Astrophotography/masters/flat/master_flat_$FILTER:%s$ -cc=dark
+load pp_light_00001
 findstar
+{platesolve_command}
 close
 """
     # Create a temp directory for Siril to use.
@@ -115,77 +121,29 @@ close
             # print(result.stdout)
             match = re.search(regex, result.stdout)
             if not match:
-                print("No match found")
-                return None, None
-            num_stars, fwhm = match.groups()
-            return int(num_stars), float(fwhm)
-        except subprocess.CalledProcessError as e:
-            return None, None
-
-def run_plate_solve_siril(this_dir, file, wcs_coords, focal_option):
-    global SIRIL_PATH
-    siril_commands = f"""requires 1.2.0
-convert light -debayer -out=.
-load light_00001
-# platesolve {wcs_coords} -platesolve -catalog=nomad {focal_option}
-platesolve {wcs_coords}
-close
-"""
-    # Create a temp directory for Siril to use.
-    with tempfile.TemporaryDirectory() as temp_dir:
-        # Copy the file to the temp directory.
-        temp_file = temp_dir + '/' + os.path.basename(file)
-        os.system(f"cp {file} {temp_file}")
-        # Define the command to run
-        siril_cli_command = [SIRIL_PATH, "-d", temp_dir, "-s", "-"]
-        # Run the command and capture output
-        try:
-            result = subprocess.run(siril_cli_command,
-                                    input=siril_commands,
-                                    text=True,
-                                    capture_output=True,
-                                    check=True)
-            # print(result.stdout)
-            # print(result.stderr)
+                print("No stars found")
+                num_stars, fwhm = None, None
+            else:
+                num_stars, fwhm = match.groups()
             ra, dec = extract_and_convert_coordinates_siril(result.stdout)
-            return ra, dec
+            return int(num_stars), float(fwhm), ra, dec
         except subprocess.CalledProcessError as e:
-            # print(f"Error running Siril: {e}")
-            return None, None
-
-def run_plate_solve_astap(this_dir, file, wcs_coords, focal_option):
-    ASTAP_PATH = '/Applications/ASTAP.app/Contents/MacOS/astap'
-    astap_cli_command = [ASTAP_PATH, "-f", file]
-    try:
-        result = subprocess.run(astap_cli_command,
-                                text=True,
-                                capture_output=True,
-                                check=True)
-        ra, dec = extract_and_convert_coordinates_astap(result.stdout)
-        return ra, dec
-    except subprocess.CalledProcessError as e:
-        return None, None
+            return None, None, None, None
 
 def load_prev_files(filename):
     if not os.path.exists(filename):
-        return [], []
-    # The file is a CSV file with several columns, with the filename in the first column.
-    filenames = []
-    star_stats = []
+        return []
+    prev_results = []
     with open(filename, 'r') as f:
         lines = f.readlines()
         for line in lines[1:]:
             # print(line)
             parts = line.split(',')
-            filenames.append(parts[0])
-            star_stats.append((float(parts[1]), int(parts[4]), float(parts[5])))
-    return filenames, star_stats
+            # Note: Row format is: Filename,CaptureTime,RA,DEC,NumStars,FWHM
+            prev_results.append((parts[0], int(parts[1]), float(parts[2]), float(parts[3]), int(parts[4]), float(parts[5])))
+    return prev_results
 
-def plot_star_stats(data):
-    # print(data)
-    # Unpack the data into two separate lists
-    times, num_stars, fwhm = zip(*data)
-
+def plot_star_stats(num_stars, fwhm, min_num_stars, max_fwhm):
     # Create a figure and a single subplot
     fig, ax1 = plt.subplots()
 
@@ -193,21 +151,26 @@ def plot_star_stats(data):
     color = 'tab:red'
     ax1.set_xlabel('Image number')
     ax1.set_ylabel('num_stars', color=color)
-    # sort num_stars in ascending order
-    num_stars = [x for x in sorted(num_stars)]
+    # sort num_stars in descending order
+    # num_stars = [x for x in sorted(num_stars, reverse=True)]
     ax1.plot(num_stars, color=color)
     ax1.tick_params(axis='y', labelcolor=color)
+    # Add a horizontal line for the minimum number of stars
+    if min_num_stars > 0:
+        ax1.axhline(y=min_num_stars, color='r', linestyle='--')
 
     # Create a second y-axis for FWHM
     ax2 = ax1.twinx()
     color = 'tab:blue'
     ax2.set_ylabel('FWHM', color=color)
     # sort fwhm in ascending order
-    fwhm = [x for x in sorted(fwhm)]
+    # fwhm = [x for x in sorted(fwhm)]
     ax2.plot(fwhm, color=color)
     ax2.tick_params(axis='y', labelcolor=color)
     # Set Y axis labels to show 2 decimal places.
     ax2.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{x:.2f}'))
+    if max_fwhm > 0:
+        ax2.axhline(y=max_fwhm, color='b', linestyle='--')
 
     # Set the title.
     plt.title('Number of stars detected and FWHM')
@@ -256,34 +219,33 @@ def get_image_capture_time(image_file):
     else:
         return get_raw_image_capture_time(image_file)
 
-def process_file(current_dir, coordinates, focal_option, filename, star_stats, csv_file, lock):
+def process_file(current_dir, coordinates, focal_option, filename, results, bad_files, csv_file, lock):
     # print(f"Processing {filename}")
     t_start = time.time()
     filename_without_path = os.path.basename(filename)
     capture_time = int(get_image_capture_time(filename).timestamp())
-    if coordinates is None:
-        result = run_plate_solve_astap(current_dir, filename, coordinates, focal_option)
-    else:
-        result = run_plate_solve_siril(current_dir, filename, coordinates, focal_option)
-    if result[0] is not None and result[1] is not None:
-        # print(f"RA={result[0]:.12f}, DEC={result[1]:.12f}")
-        pass
-    else:
-        print(f"{filename_without_path} [Platesolve failed]")
+    num_stars, fwhm, ra, dec = findstar_and_platesolve_siril(coordinates, filename)
+    if num_stars is None or fwhm is None or ra is None or dec is None:
+        print(f"{filename_without_path} [Image analysis failed]")
+        bad_files.append(filename_without_path)
         return
-    num_stars, fwhm = run_star_detect_siril(current_dir, filename)
-    if num_stars is None:
-        num_stars = 0
-    if fwhm is None:
-        fwhm = 0
     analysis_time = time.time() - t_start
-    print(f"{filename_without_path} CaptureTime={capture_time:10d}, RA={result[0]:.12f}, DEC={result[1]:.12f}, NumStars={num_stars:5d}, FWHM={fwhm:.3f}, AnalysisTime={analysis_time:.2f}s")
+    print(f"{filename_without_path} CaptureTime={capture_time:10d}, RA={ra:.12f}, DEC={dec:.12f}, NumStars={num_stars:5d}, FWHM={fwhm:.3f}, AnalysisTime={analysis_time:.2f}s")
     with lock:
-        star_stats.append((capture_time, int(num_stars), float(fwhm)))
+        results.append((filename_without_path, capture_time, ra, dec, num_stars, fwhm))
         if csv_file is not None:
             with open(csv_file, 'a') as f:
-                f.write(f"{filename_without_path}, {capture_time:10d}, {result[0]:.12f}, {result[1]:.12f}, {num_stars}, {fwhm}\n")
+                f.write(f"{filename_without_path}, {capture_time:10d}, {ra:.12f}, {dec:.12f}, {num_stars}, {fwhm}\n")
 
+def filter_subs(results, min_num_stars, max_fwhm):
+    culled_files = [x[0] for x in results if x[4] < min_num_stars or x[5] > max_fwhm]
+    if len(culled_files) == 0:
+        print("No files to cull")
+        return
+    print(f"Recommend culling {len(culled_files)} files out of {len(results)} ({len(culled_files)/len(results)*100:.2f}%)")
+    for f in culled_files:
+        print(f, end=' ')
+    print()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Platesolve all images in a directory')
@@ -292,16 +254,14 @@ if __name__ == "__main__":
     parser.add_argument('-w', '--wcs', type=str, help='WCS coordinates')
     parser.add_argument('-f', '--focal', type=str, help='Override focal length', default='')
     parser.add_argument('-c', '--csv', type=str, help='CSV file to write results to', default='')
+    parser.add_argument('-s', '--min-num-stars', type=int, help='Minimum number of stars for filtering', default=0)
+    parser.add_argument('-m', '--max-fwhm', type=float, help='Maximum FWHM for filtering', default=10)
     args = parser.parse_args()
     coordinates = None
     if args.directory is None:
         print('ERROR: No directory specified')
         parser.print_help()
         sys.exit(1)
-    # if args.object is None and args.wcs is None:
-    #     print('ERROR: No object or WCS coordinates specified')
-    #     parser.print_help()
-    #     sys.exit(1)
     if args.object is not None and args.wcs is not None:
         print('ERROR: Both object and WCS coordinates specified')
         parser.print_help()
@@ -318,25 +278,17 @@ if __name__ == "__main__":
     else:
         focal_option = ''
 
-
     if coordinates is None:
-        print('\nWARNING!\nBlind platesolving using ASTAP, no WCS coordinates specified, and no object name specified -- this will be slow! \nIf you know the approximate RA and DEC of the image, specify it with the -w option, or specify an object name with the -o option.\n')
+        print('\nWARNING!\nBlind platesolving, no WCS coordinates specified, and no object name specified -- this may be inaccurate! \nIf you know the approximate RA and DEC of the image, specify it with the -w option, or specify an object name with the -o option.\n')
 
-    prev_filenames, star_stats = load_prev_files(args.csv)
-    if len(prev_filenames) > 0:
-        print(f"Skipping {len(prev_filenames)} files that have already been platesolved")
-        if False:
-            n = len(prev_filenames)
-            for i in range(n):
-                print(f"{prev_filenames[i]}: {star_stats[i]}")
-            exit(1)
+    prev_results = load_prev_files(args.csv)
 
     current_dir = os.getcwd()
     csv_filename = None
     if args.csv != '':
         csv_filename = args.csv
         csv_file = open(args.csv, 'a')
-        if len(prev_filenames) == 0:
+        if len(prev_results) == 0:
             csv_file.write('Filename,CaptureTime,RA,DEC,NumStars,FWHM\n')
         print(f"Writing results to {args.csv}")
         csv_file.close()
@@ -349,20 +301,39 @@ if __name__ == "__main__":
     print(f"Processing {len(files)} files in directory {args.directory}")
 
     new_files = []
+    if len(prev_results) > 0:
+        prev_filenames = [x[0] for x in prev_results]
+    else:
+        prev_filenames = []
     for filename in files:
         if filename in prev_filenames:
             print(f"{filename} [Previously solved, skipping]")
-            # remove the filename from the list of files to process
         else:
             new_files.append(filename)
     # Append the directory to the filenames
     files = [args.directory + '/' + f for f in new_files]
-    lock = multiprocessing.Lock()
 
+    results = prev_results
+    lock = multiprocessing.Lock()
     with multiprocessing.Pool(10) as pool:
         m = multiprocessing.Manager()
+        bad_files = m.list()
         l = m.Lock()
-        pool.starmap(process_file, [(current_dir, coordinates, focal_option, f, star_stats, csv_filename, l) for f in files])
+        pool.starmap(process_file, [(current_dir, coordinates, focal_option, f, results, bad_files, csv_filename, l) for f in files])
 
-    star_stats.sort(key=lambda x: x[0])
-    plot_star_stats(star_stats)
+    if len(bad_files) > 0:
+        print(f"{len(bad_files)} files failed to platesolve:")
+        for f in bad_files:
+            print(f, end=' ')
+        print()
+
+    # Sort the results by capture time.
+    results.sort(key=lambda x: x[1])
+    filter_subs(results, args.min_num_stars, args.max_fwhm)
+
+    num_stars = [x[4] for x in results]
+    fwhm = [x[5] for x in results]
+    # print(num_stars)
+    # print(fwhm)
+    plot_star_stats(num_stars, fwhm, args.min_num_stars, args.max_fwhm)
+
